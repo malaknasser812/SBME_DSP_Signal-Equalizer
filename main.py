7981aa5e9a40864d36a926dda3d2b69fafda7f2c
fou65 <115308809+fou65@users.noreply.github.com>
2023-11-11 13:59:45 +0200

    new ui and new code from farah

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from scipy.fft import fft
 import scipy.signal as sig
 from scipy import interpolate
@@ -11,8 +10,8 @@ import time
 from scipy.interpolate import interp1d
 import pyqtgraph as pg
 from matplotlib.figure import Figure
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QFileDialog, QMessageBox, QGraphicsScene ,QLabel , QHBoxLayout
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QSlider, QVBoxLayout, QGraphicsScene ,QLabel , QHBoxLayout ,QComboBox ,QGroupBox 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from PyQt5 import QtWidgets, uic 
 from matplotlib.pyplot import figure
@@ -21,8 +20,20 @@ from cmath import*
 from numpy import *
 import sys
 import matplotlib
-
 matplotlib.use('Qt5Agg')
+
+class CreateSlider:
+    def __init__(self):
+        # Create a slider
+        self.slider = QSlider()
+        #sets the orientation of the slider to be vertical.
+        self.slider.setOrientation(QtCore.Qt.Orientation.Vertical)
+        self.slider.setTickPosition(QSlider.TicksBothSides)
+        self.slider.setTickInterval(10)
+        self.slider.setSingleStep(1)
+
+    def get_slider(self):
+        return self.slider
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -31,12 +42,48 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Load the UI Page
         uic.loadUi(r'task3.ui', self)
-
-
+        self.selected_mode = None
+        self.frame_layout = QHBoxLayout(self.sliders_frame)
+        # Connect the signal to set_combobox
+        self.set_combobox()
+        # Connect the activated signal to a custom slot
+        self.modes_combobox.activated.connect(lambda: self.combobox_activated())
+        
 #YOUR CODE HERE 
 # بسم الله الرحمن الرحييم
+    def set_combobox(self):
+        self.modes_combobox.addItem('Uniform Range')
+        self.modes_combobox.addItem('Musical Instruments')
+        self.modes_combobox.addItem('Animal Sounds')
+        self.modes_combobox.addItem('ECG Abnormalities')
 
+    def combobox_activated(self):
+        # Get the selected item's text and display it in the label
+        selected_text = self.modes_combobox.currentIndex()
+        # store the mode in a global variable 
+        self.selected_mode = selected_text 
+        print(selected_text)
+        self.add_slider(selected_text)
+        
+    def clear_layout(self ,layout):
+        for i in reversed(range(layout.count())):
+            item = layout.itemAt(i)
+            if item.widget():
+                item.widget().deleteLater() 
 
+    def add_slider(self , selected_text):          
+        if selected_text == 0:
+            self.clear_layout(self.frame_layout)
+            for _ in range(10):
+                slider_creator = CreateSlider()
+                slider = slider_creator.get_slider()
+                self.frame_layout.addWidget(slider)  
+        else:
+            self.clear_layout(self.frame_layout) 
+            for _ in range(4):
+                slider_creator = CreateSlider()
+                slider = slider_creator.get_slider()
+                self.frame_layout.addWidget(slider)
 def main():
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
